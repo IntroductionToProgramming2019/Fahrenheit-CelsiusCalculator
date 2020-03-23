ee29de5e5e3f37ffab108b0fdcd3b0c6d564e1d7
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 17:07:29 +0000

    Add project files.

---
+++ fahrenheit to celcius/Program.cs
@@ -0,0 +1,26 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : simple Fahrenheit to Celsius calculator
+ * 
+ * ############################# */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fahrenheit_to_celcius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine("Please enter degrees Fahrenheit: " );
+            int Fahrenheit = int.Parse(Console.ReadLine());
+            int Celsius = 5*(Fahrenheit - 32) / 9;
+            Console.WriteLine(Celsius + "  degrees");
+        }
+    }
+}
